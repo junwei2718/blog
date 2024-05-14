48db5023a4c89f56e3dedf00581ac0b8a6288e2b
DESKTOP-O5F1T1A\Administrator <123@qq.com>
2024-05-14 11:31:59 +0800

    添加reg接口

--- user/views.py
+++ user/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+# from django.shortcuts import render
 # Create your views here.
+from django.http import HttpRequest, HttpResponse,HttpResponseBadRequest, JsonResponse
+import simplejson
+
+def reg(request: HttpRequest):
+    print(request.body)
+    print(request.headers)
+    print("nihao!")
+    return JsonResponse({"message": "提交成功！"})
